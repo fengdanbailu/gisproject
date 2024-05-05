12ee8c451840556d589bd8975d5fd9a29b14703f
gmchen <chenguoming2015@163.com>
2024-05-05 09:17:39 +0800

    init: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+
+Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_TOKEN;
+
+onMounted(() => {
+  const viewr = new Cesium.Viewer("cesiumContainer");
+});
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
